9dc7b7c802978acead594390a35979b747340f3e
Tomislav Radanović <tommy.radan@gmail.com>
2016-10-05 01:58:48 +0200

    Remove GLM

---
+++ Core/Mathematics/Vector2.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace Math
+{
+	struct Vector2
+	{
+		Vector2(float x = 0.0f, float y = 0.0f) : X( x ), Y( y ) {}
+
+		Vector2& operator+=(const Vector2& v);
+		Vector2& operator-=(const Vector2& v);
+
+		const Vector2 operator+(const Vector2& v) const;
+		const Vector2 operator-(const Vector2& v) const;
+
+		friend Vector2 operator*(const Vector2& v, float n);
+		friend Vector2 operator*(float n, const Vector2& v);
+
+		friend Vector2 operator/(const Vector2& v, float n);
+		friend Vector2 operator/(float n, const Vector2& v);
+
+		float& operator[](const int i);
+
+		const float Dot(const Vector2& v) const;
+		const float Angle(const Vector2& v) const;
+
+		const float LengthSqr(void) const;
+		const float Length(void) const;
+		const float Distance(const Vector2& v) const;
+
+		const Vector2 Normal() const;
+
+		float X, Y;
+	};
+}
