c107c20403a37e73c20194b978162f8847789686
Tomislav Radnović <tommy.radan@gmail.com>
2016-10-08 00:45:43 +0200

    Add bare loop

--- Core/Control/MainLoop.cpp
+++ Core/Control/MainLoop.cpp
@@ -1,6 +1,24 @@
 #include <MediaLayer\SDL\SDL.h>
 
+#include <MediaLayer\Display.hpp>
+#include <MediaLayer\Events.hpp>
+
 int main(int argc, char* argv[])
 {
+	Display* display = new Display();
+	Events* events = new Events();
+
+	for (;;)
+	{
+		events->Process();
+		if (events->IsQuitRequested()) break;
+
+
+		
+		display->SwapBuffers();
+	}
+
+	delete events;
+	delete display;
     return 0;
 }
