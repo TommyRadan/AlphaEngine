60c12f5620ca996df447cb12a174b26885d522b0
Tomislav Radnović <tommy.radan@gmail.com>
2016-10-07 21:47:53 +0200

    Put OGL class under OpenGL namespace

--- Core/RenderingEngine/OpenGL/OpenGL.cpp
+++ Core/RenderingEngine/OpenGL/OpenGL.cpp
@@ -1,89 +1,92 @@
 #include "OpenGL.hpp"
 
-OGL::OGL()
+namespace OpenGL
 {
-	GLenum status = glewInit();
-	if (status != GLEW_OK) {
-		throw Exception("Could not gather OpenGL function pointers!");
+	OGL::OGL()
+	{
+		GLenum status = glewInit();
+		if (status != GLEW_OK) {
+			throw Exception("Could not gather OpenGL function pointers!");
+		}
+
+		int majorVersion = 0;
+		int minorVersion = 0;
+		glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+		glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+
+		if (majorVersion < 3 || (majorVersion == 3 && minorVersion < 3)) {
+			throw Exception("OpenGL 3.3 is the oldest version supported!");
+		}
 	}
 
-	int majorVersion = 0;
-	int minorVersion = 0;
-	glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
-	glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
-
-	if (majorVersion < 3 || (majorVersion == 3 && minorVersion < 3)) {
-		throw Exception("OpenGL 3.3 is the oldest version supported!");
+	void OGL::Enable(const OpenGL::Capability capability)
+	{
+		glEnable((GLenum)capability);
 	}
-}
 
-void OGL::Enable(const OpenGL::Capability capability)
-{
-	glEnable((GLenum)capability);
-}
+	void OGL::Disable(const OpenGL::Capability capability)
+	{
+		glDisable((GLenum)capability);
+	}
 
-void OGL::Disable(const OpenGL::Capability capability)
-{
-	glDisable((GLenum)capability);
-}
+	void OGL::ClearColor(const Color& col)
+	{
+		glClearColor(col.R / 255.0f, col.G / 255.0f, col.B / 255.0f, col.A / 255.0f);
+	}
 
-void OGL::ClearColor(const Color& col)
-{
-	glClearColor(col.R / 255.0f, col.G / 255.0f, col.B / 255.0f, col.A / 255.0f);
-}
+	void OGL::Clear(const OpenGL::Buffer buffers)
+	{
+		glClear((GLbitfield)buffers);
+	}
 
-void OGL::Clear(const OpenGL::Buffer buffers)
-{
-	glClear((GLbitfield)buffers);
-}
+	void OGL::DepthMask(const bool writeEnabled)
+	{
+		glDepthMask(writeEnabled ? GL_TRUE : GL_FALSE);
+	}
 
-void OGL::DepthMask(const bool writeEnabled)
-{
-	glDepthMask(writeEnabled ? GL_TRUE : GL_FALSE);
-}
+	void OGL::BindTexture(const OpenGL::Texture& texture, const unsigned char unit)
+	{
+		glActiveTexture(GL_TEXTURE0 + unit);
+		glBindTexture(GL_TEXTURE_2D, texture.Handle());
+	}
 
-void OGL::BindTexture(const OpenGL::Texture& texture, const unsigned char unit)
-{
-	glActiveTexture(GL_TEXTURE0 + unit);
-	glBindTexture(GL_TEXTURE_2D, texture.Handle());
-}
+	void OGL::BindFramebuffer(const OpenGL::Framebuffer& framebuffer)
+	{
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, framebuffer.Handle());
 
-void OGL::BindFramebuffer(const OpenGL::Framebuffer& framebuffer)
-{
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, framebuffer.Handle());
+		// Set viewport to frame buffer size
+		GLint obj, width, height;
+		glGetFramebufferAttachmentParameteriv(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME, &obj);
 
-	// Set viewport to frame buffer size
-	GLint obj, width, height;
-	glGetFramebufferAttachmentParameteriv(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME, &obj);
+		GLint res;
+		glGetIntegerv(GL_TEXTURE_BINDING_2D, &res);
+		glBindTexture(GL_TEXTURE_2D, obj);
+		glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &width);
+		glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &height);
+		glBindTexture(GL_TEXTURE_2D, res);
 
-	GLint res;
-	glGetIntegerv(GL_TEXTURE_BINDING_2D, &res);
-	glBindTexture(GL_TEXTURE_2D, obj);
-	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &width);
-	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &height);
-	glBindTexture(GL_TEXTURE_2D, res);
+		glViewport(0, 0, width, height);
+	}
 
-	glViewport(0, 0, width, height);
-}
+	void OGL::BindFramebuffer(void)
+	{
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 
-void OGL::BindFramebuffer(void)
-{
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+		// Set viewport to default frame buffer size
+		//glViewport(m_DefaultViewport[0], m_DefaultViewport[1], m_DefaultViewport[2], m_DefaultViewport[3]);
+	}
 
-	// Set viewport to default frame buffer size
-	//glViewport(m_DefaultViewport[0], m_DefaultViewport[1], m_DefaultViewport[2], m_DefaultViewport[3]);
-}
+	void OGL::DrawArrays(const OpenGL::VertexArray& vao, const OpenGL::Primitive mode, const unsigned int offset, const unsigned int vertices)
+	{
+		glBindVertexArray(vao.Handle());
+		glDrawArrays((GLenum)mode, offset, vertices);
+		glBindVertexArray(0);
+	}
 
-void OGL::DrawArrays(const OpenGL::VertexArray& vao, const OpenGL::Primitive mode, const unsigned int offset, const unsigned int vertices)
-{
-	glBindVertexArray(vao.Handle());
-	glDrawArrays((GLenum)mode, offset, vertices);
-	glBindVertexArray(0);
-}
-
-void OGL::DrawElements(const OpenGL::VertexArray& vao, const OpenGL::Primitive mode, const intptr_t offset, const unsigned int count, const unsigned int type)
-{
-	glBindVertexArray(vao.Handle());
-	glDrawElements((GLenum)mode, count, type, (const GLvoid*)offset);
-	glBindVertexArray(0);
+	void OGL::DrawElements(const OpenGL::VertexArray& vao, const OpenGL::Primitive mode, const intptr_t offset, const unsigned int count, const unsigned int type)
+	{
+		glBindVertexArray(vao.Handle());
+		glDrawElements((GLenum)mode, count, type, (const GLvoid*)offset);
+		glBindVertexArray(0);
+	}
 }
--- Core/RenderingEngine/OpenGL/OpenGL.hpp
+++ Core/RenderingEngine/OpenGL/OpenGL.hpp
@@ -16,25 +16,28 @@
 #include <Utilities\Exceptions\Exception.hpp>
 #include <Utilities\Singleton.hpp>
 
-class OGL : public Singleton<OGL>
+namespace OpenGL
 {
-	friend Singleton<OGL>;
-	OGL();
+	class OGL : public Singleton<OGL>
+	{
+		friend Singleton<OGL>;
+		OGL();
 
-public:
-	void Enable(const OpenGL::Capability capability);
-	void Disable(const OpenGL::Capability capability);
+	public:
+		void Enable(const OpenGL::Capability capability);
+		void Disable(const OpenGL::Capability capability);
 
-	void ClearColor(const Color& col);
-	void Clear(const OpenGL::Buffer buffers = OpenGL::Buffer::Color | OpenGL::Buffer::Depth);
+		void ClearColor(const Color& col);
+		void Clear(const OpenGL::Buffer buffers = OpenGL::Buffer::Color | OpenGL::Buffer::Depth);
 
-	void DepthMask(const bool writeEnabled);
+		void DepthMask(const bool writeEnabled);
 
-	void BindTexture(const OpenGL::Texture& texture, const unsigned char unit);
+		void BindTexture(const OpenGL::Texture& texture, const unsigned char unit);
 
-	void BindFramebuffer(const OpenGL::Framebuffer& framebuffer);
-	void BindFramebuffer(void);
+		void BindFramebuffer(const OpenGL::Framebuffer& framebuffer);
+		void BindFramebuffer(void);
 
-	void DrawArrays(const OpenGL::VertexArray& vao, const OpenGL::Primitive mode, const unsigned int offset, const unsigned int vertices);
-	void DrawElements(const OpenGL::VertexArray& vao, const OpenGL::Primitive mode, const intptr_t offset, const unsigned int count, const unsigned int type);
-};
+		void DrawArrays(const OpenGL::VertexArray& vao, const OpenGL::Primitive mode, const unsigned int offset, const unsigned int vertices);
+		void DrawElements(const OpenGL::VertexArray& vao, const OpenGL::Primitive mode, const intptr_t offset, const unsigned int count, const unsigned int type);
+	};
+}
