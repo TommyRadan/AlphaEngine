f0977578a08cbb450420f0baeb5102916ef16717
Tomislav Radnović <tommy.radan@gmail.com>
2016-10-10 01:38:28 +0200

    Add StandardRenderer::Init and StandardRenderer::Quit

--- Core/RenderingEngine/Renderer.hpp
+++ Core/RenderingEngine/Renderer.hpp
@@ -11,86 +11,89 @@
 class Renderer
 {
 protected:
-	Renderer() :
-		m_VertexShader(OpenGL::ShaderType::Vertex),
-		m_FragmentShader(OpenGL::ShaderType::Fragment)
+	Renderer(void) :
+		m_IsInit{ false }
 	{}
 
-	OpenGL::Shader m_VertexShader;
-	OpenGL::Shader m_FragmentShader;
-	OpenGL::Program m_Program;
+	virtual void Init(void) = 0;
+	virtual void Quit(void) = 0;
+	bool m_IsInit;
+
+	OpenGL::Shader* m_VertexShader;
+	OpenGL::Shader* m_FragmentShader;
+	OpenGL::Program* m_Program;
 
 public:
 	void StartRenderer(void)
 	{
-		m_Program.Start();
+		m_Program->Start();
 	}
 
 	void UploadTextureReference(const std::string& textureName, const int position)
 	{
-		OpenGL::Uniform uniform = m_Program.GetUniform(textureName);
+		OpenGL::Uniform uniform = m_Program->GetUniform(textureName);
 		if (uniform == -1) {
 			return;
 		}
-		m_Program.SetUniform(uniform, position);
+		m_Program->SetUniform(uniform, position);
 	}
 
 	void UploadCoefficient(const std::string& coefficientName, const float coefficient)
 	{
-		OpenGL::Uniform uniform = m_Program.GetUniform(coefficientName);
+		OpenGL::Uniform uniform = m_Program->GetUniform(coefficientName);
 		if (uniform == -1) {
 			return;
 		}
-		m_Program.SetUniform(uniform, coefficient);
+		m_Program->SetUniform(uniform, coefficient);
 	}
 
 	void UploadMatrix3(const std::string& mat3Name, const Math::Matrix3& matrix)
 	{
-		OpenGL::Uniform uniform = m_Program.GetUniform(mat3Name);
+		OpenGL::Uniform uniform = m_Program->GetUniform(mat3Name);
 		if (uniform == -1) {
 			return;
 		}
-		m_Program.SetUniform(uniform, matrix);
+		m_Program->SetUniform(uniform, matrix);
 	}
 
 	void UploadMatrix4(const std::string& mat4Name, const Math::Matrix4& matrix)
 	{
-		OpenGL::Uniform uniform = m_Program.GetUniform(mat4Name);
+		OpenGL::Uniform uniform = m_Program->GetUniform(mat4Name);
 		if (uniform == -1) {
 			return;
 		}
-		m_Program.SetUniform(uniform, matrix);
+		m_Program->SetUniform(uniform, matrix);
 	}
 
 	void UploadVector2(const std::string& vec2Name, const Math::Vector2& vector)
 	{
-		OpenGL::Uniform uniform = m_Program.GetUniform(vec2Name);
+		OpenGL::Uniform uniform = m_Program->GetUniform(vec2Name);
 		if (uniform == -1) {
 			return;
 		}
-		m_Program.SetUniform(uniform, vector);
+		m_Program->SetUniform(uniform, vector);
 	}
 
 	void UploadVector3(const std::string& vec3Name, const Math::Vector3& vector)
 	{
-		OpenGL::Uniform uniform = m_Program.GetUniform(vec3Name);
+		OpenGL::Uniform uniform = m_Program->GetUniform(vec3Name);
 		if (uniform == -1) {
 			return;
 		}
-		m_Program.SetUniform(uniform, vector);
+		m_Program->SetUniform(uniform, vector);
 	}
 
 	void UploadVector4(const std::string& vec4Name, const Math::Vector4& vector)
 	{
-		OpenGL::Uniform uniform = m_Program.GetUniform(vec4Name);
+		OpenGL::Uniform uniform = m_Program->GetUniform(vec4Name);
 		if (uniform == -1) {
 			return;
 		}
-		m_Program.SetUniform(uniform, vector);
+		m_Program->SetUniform(uniform, vector);
 	}
 
 	void StopRenderer(void)
 	{
-		m_Program.Stop();
+		m_Program->Stop();
 	}
 };
--- Core/RenderingEngine/StandardRenderer/StandardRenderer.hpp
+++ Core/RenderingEngine/StandardRenderer/StandardRenderer.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include <Utilities\Exception.hpp>
 #include <Utilities\FileToString.hpp>
+#include <Utilities\Singleton.hpp>
 
 #include <Mathematics\Math.hpp>
 
-#include <Utilities\Singleton.hpp>
 #include <RenderingEngine\Renderer.hpp>
 
 class StandardRenderer : 
@@ -13,18 +14,44 @@ class StandardRenderer :
 {
 	friend Singleton<StandardRenderer>;
 	StandardRenderer(void)
+	{}
+
+	void Init(void) override
 	{
+		if (m_IsInit) {
+			throw Exception("Called StandardRenderer::Init twice");
+		}
+
+		m_VertexShader = new OpenGL::Shader(OpenGL::ShaderType::Vertex);
+		m_FragmentShader = new OpenGL::Shader(OpenGL::ShaderType::Fragment);
+		m_Program = new OpenGL::Program();
+
 		std::string vertexCode = FileToString(std::string("StandardShader.vs"));
 		std::string fragmentCode = FileToString(std::string("StandardShader.fs"));
 
-		m_VertexShader.Source(vertexCode);
-		m_VertexShader.Compile();
+		m_VertexShader->Source(vertexCode);
+		m_VertexShader->Compile();
 
-		m_FragmentShader.Source(fragmentCode);
-		m_FragmentShader.Compile();
+		m_FragmentShader->Source(fragmentCode);
+		m_FragmentShader->Compile();
 
-		m_Program.Attach(m_VertexShader);
-		m_Program.Attach(m_FragmentShader);
-		m_Program.Link();
+		m_Program->Attach(*m_VertexShader);
+		m_Program->Attach(*m_FragmentShader);
+		m_Program->Link();
+
+		m_IsInit = true;
+	}
+
+	void Quit(void) override
+	{
+		if (!m_IsInit) {
+			throw Exception("Called StandardRenderer::Quit before StandardRenderer::Init");
+		}
+
+		delete m_VertexShader;
+		delete m_FragmentShader;
+		delete m_Program;
+
+		m_IsInit = false;
 	}
 };
