1e950908deced85204686f223919617ff59070c3
Tino Melvan <tmelva00@fesb.hr>
2016-11-25 18:26:33 +0100

    Changes to vector4.hpp

--- Core/Mathematics/Vector4.cpp
+++ Core/Mathematics/Vector4.cpp
@@ -3,7 +3,12 @@
 
 namespace Math
 {
-	Vector4& Vector4::operator+=( const Vector4& v )
+	float DotProduct(const Vector4& v1, const Vector4& v2)
+	{
+		return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z + v1.W * v2.W;
+	}
+
+	Vector4& Vector4::operator+=(const Vector4& v)
 	{
 		X += v.X;
 		Y += v.Y;
@@ -12,7 +17,7 @@ namespace Math
 		return *this;
 	}
 
-	Vector4& Vector4::operator-=( const Vector4& v )
+	Vector4& Vector4::operator-=(const Vector4& v)
 	{
 		X -= v.X;
 		Y -= v.Y;
@@ -21,33 +26,119 @@ namespace Math
 		return *this;
 	}
 
-	const Vector4 Vector4::operator+( const Vector4& v ) const
+	const Vector4 Vector4::operator+(const Vector4& v) const
 	{
-		return Vector4( X + v.X, Y + v.Y, Z + v.Z, W + v.W );
+		return Vector4(X + v.X, Y + v.Y, Z + v.Z, W + v.W);
 	}
 
-	const Vector4 Vector4::operator-( const Vector4& v ) const
+	const Vector4 Vector4::operator-(const Vector4& v) const
 	{
-		return Vector4( X - v.X, Y - v.Y, Z - v.Z, W - v.W );
+		return Vector4(X - v.X, Y - v.Y, Z - v.Z, W - v.W);
 	}
 
-	Vector4 operator*( const Vector4& v, float n )
-	{
-		return Vector4( v.X * n, v.Y * n, v.Z * n, v.W * n );
+	const Vector4 Vector4::operator/(const Vector4& v) const {
+		return *this / v.GetVectorLength();
 	}
 
-	Vector4 operator*( float n, const Vector4& v )
+	Vector4& Vector4::operator/=(const Vector4& v)
 	{
-		return Vector4( v.X * n, v.Y * n, v.Z * n, v.W * n );
+		float l = v.GetVectorLength();
+
+		X /= l;
+		Y /= l;
+		Z /= l;
+		W /= l;
+
+		return *this;
 	}
 
-	Vector4 operator/( const Vector4& v, float n )
+	Vector4 operator+(const Vector4& v, float n)
 	{
-		return Vector4( v.X / n, v.Y / n, v.Z / n, v.W / n );
+		return Vector4(v.X + n, v.Y + n, v.Z + n, v.W + n);
 	}
 
-	Vector4 operator/( float n, const Vector4& v )
+	Vector4 operator-(const Vector4& v, float n)
 	{
-		return Vector4( v.X / n, v.Y / n, v.Z / n, v.W / n );
+		return Vector4(v.X - n, v.Y - n, v.Z - n, v.W - n);
+	}
+
+	Vector4 operator+(float n, const Vector4& v)
+	{
+		return Vector4(n + v.X, n + v.Y, n + v.Z, n + v.W);
+	}
+
+	Vector4 operator-(float n, const Vector4& v)
+	{
+		return Vector4(n - v.X, n - v.Y, n - v.Z, n - v.W);
+	}
+
+	Vector4& Vector4::operator+=(float n)
+	{
+		X += n;
+		Y += n;
+		Z += n;
+		W += n;
+		return *this;
+	}
+
+	Vector4& Vector4::operator-=(float n)
+	{
+		X -= n;
+		Y -= n;
+		Z -= n;
+		W -= n;
+		return *this;
+	}
+
+	Vector4 operator*(const Vector4& v, float n)
+	{
+		return Vector4(v.X * n, v.Y * n, v.Z * n, v.W * n);
+	}
+
+	Vector4 operator/(const Vector4& v, float n)
+	{
+		return Vector4(v.X / n, v.Y / n, v.Z / n, v.W / n);
+	}
+
+	Vector4 operator*(float n, const Vector4& v)
+	{
+		return Vector4(n * v.X, n * v.Y, n * v.Z, n * v.W);
+	}
+
+	Vector4 operator/(float n, const Vector4& v)
+	{
+		return Vector4(n / v.X, n / v.Y, n / v.Z, n / v.W);
+	}
+
+	Vector4& Vector4::operator*=(float n)
+	{
+		X *= n;
+		Y *= n;
+		Z *= n;
+		W *= n;
+		return *this;
+	}
+
+	Vector4& Vector4::operator/=(float n)
+	{
+		X /= n;
+		Y /= n;
+		Z /= n;
+		W /= n;
+		return *this;
+	}
+
+	bool Vector4::operator==(const Vector4& v)
+	{
+		if (X == v.X && Y == v.Y && Z == v.Z && W == v.W)
+			return true;
+		return false;
+	}
+
+	bool Vector4::operator!=(const Vector4& v)
+	{
+		if (X == v.X && Y == v.Y && Z == v.Z && W == v.W)
+			return false;
+		return true;
 	}
 }
--- Core/Mathematics/Vector4.hpp
+++ Core/Mathematics/Vector4.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Vector3.hpp"
+#include <cmath>
 
 namespace Math
 {
@@ -9,17 +10,42 @@ namespace Math
 		Vector4( float x = 0.0f, float y = 0.0f, float z = 0.0f, float w = 1.0f ) : X( x ), Y( y ), Z( z ), W( w ) {}
 		Vector4( const Vector3& v, float w = 1.0f ) : X( v.X ), Y( v.Y ), Z( v.Z ), W( w ) {}
 
-		Vector4& operator+=( const Vector4& v );
-		Vector4& operator-=( const Vector4& v );
+		const float GetVectorLength() const {
+			return sqrt(X*X + Y*Y + Z*Z + W*W);
+		}
+		friend float DotProduct(const Vector4& v1, const Vector4& v2);
 
-		const Vector4 operator+( const Vector4& v ) const;
-		const Vector4 operator-( const Vector4& v ) const;
+		// Vector-Vector operations
+		Vector4& operator+=(const Vector4& v);
+		Vector4& operator-=(const Vector4& v);
 
-		friend Vector4 operator*( const Vector4& v, float n );
-		friend Vector4 operator*( float n, const Vector4& v );
+		const Vector4 operator+(const Vector4& v) const;
+		const Vector4 operator-(const Vector4& v) const;
 
-		friend Vector4 operator/( const Vector4& v, float n );
-		friend Vector4 operator/( float n, const Vector4& v );
+		const Vector4 operator/(const Vector4& v) const;
+		Vector4& operator/=(const Vector4& v);
+
+		bool operator==(const Vector4& v);
+		bool operator!=(const Vector4& v);
+
+		// Vector-Scalar operations
+		friend Vector4 operator+(const Vector4& v, float n);
+		friend Vector4 operator-(const Vector4& v, float n);
+
+		friend Vector4 operator+(float n, const Vector4& v);
+		friend Vector4 operator-(float n, const Vector4& v);
+
+		Vector4& operator+=(float n);
+		Vector4& operator-=(float n);
+
+		friend Vector4 operator*(const Vector4& v, float n);
+		friend Vector4 operator/(const Vector4& v, float n);
+
+		friend Vector4 operator*(float n, const Vector4& v);
+		friend Vector4 operator/(float n, const Vector4& v);
+
+		Vector4& operator*=(float n);
+		Vector4& operator/=(float n);
 
 		// Data
 		float X, Y, Z, W;
