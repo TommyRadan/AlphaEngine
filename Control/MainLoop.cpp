2fc4a9ef375d12c48e1d88da8e7b44d66abbc77d
Tomislav Radanović <tommy.radan@gmail.com>
2017-02-15 00:53:13 +0100

    Add debug code to invoke some features

--- Control/MainLoop.cpp
+++ Control/MainLoop.cpp
@@ -9,24 +9,56 @@ int main(int argc, char* argv[])
 	(void) argv;
 
 	try {
-		MediaLayer::GetInstance()->Init();
+		MediaLayer::Context::GetInstance()->Init();
 		RenderingEngine::GetInstance()->Init();
 	} catch (Exception e) {
-		MediaLayer::GetInstance()->ShowDialog("Initialization error!", e.what());
+		MediaLayer::Context::GetInstance()->ShowDialog("Initialization error!", e.what());
 		return 1;
 	}
+
+	OpenGL::Shader vert(OpenGL::ShaderType::Vertex);
+	vert.Source("#version 150\nin vec2 position; void main() { gl_Position = vec4( position, 0.0, 1.0 ); }");
+	vert.Compile();
+
+	OpenGL::Shader frag(OpenGL::ShaderType::Fragment );
+	frag.Source("#version 150\nout vec4 outColor; void main() { outColor = vec4( 1.0, 0.0, 0.0, 1.0 ); }");
+	frag.Compile();
+
+	OpenGL::Program program;
+	program.Attach(vert);
+	program.Attach(frag);
+	program.Link();
+
+	float vertices[] = {
+			-0.5f,  0.5f,
+			0.5f,  0.5f,
+			0.5f, -0.5f
+	};
+	OpenGL::VertexBuffer vbo;
+	vbo.Data(vertices, sizeof(vertices), OpenGL::BufferUsage::StaticDraw);
+
+	OpenGL::VertexArray vao;
+	vao.BindAttribute(program.GetAttribute("position"), vbo, OpenGL::Type::Float, 2, 0, 0);
 
 	try {
 		for (;;)
 		{
-			break; // Exit immediately
+			MediaLayer::Events::GetInstance()->Process();
+            if(MediaLayer::Events::GetInstance()->IsQuitRequested()) break;
+
+			OpenGL::Context::GetInstance()->ClearColor(Color(0, 0, 0, 255));
+			OpenGL::Context::GetInstance()->Clear();
+
+			OpenGL::Context::GetInstance()->DrawArrays(vao, OpenGL::Primitive::Triangles, 0, 3);
+
+            MediaLayer::Window::GetInstance()->SwapBuffers();
 		}
 	} catch (Exception e) {
-		//MediaLayer::Display::GetInstance()->ShowDialog("Runtime error!", e.what());
+		MediaLayer::Context::GetInstance()->ShowDialog("Runtime error!", e.what());
 		return 1;
 	}
 
 	RenderingEngine::GetInstance()->Quit();
-	MediaLayer::GetInstance()->Quit();
+	MediaLayer::Context::GetInstance()->Quit();
     return 0;
 }
