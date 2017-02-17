39e0760af2064ba19920420be0348e2244a19107
Tomislav Radanović <tommy.radan@gmail.com>
2017-02-17 01:55:48 +0100

    Limit OpenGL::Texture scope

--- RenderingEngine/Material.hpp
+++ RenderingEngine/Material.hpp
@@ -4,11 +4,10 @@
 #include <map>
 
 #include <Utilities/Color.hpp>
-#include "OpenGL/Texture.hpp"
 
 struct Material
 {
 	std::map<std::string, float> Coefficients;
-	std::map<std::string, OpenGL::Texture*> Textures;
+	std::map<std::string, void*> Textures;
 	std::map<std::string, Color> Colors;
 };
