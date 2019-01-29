038c170fb953a468590dd527b5439928b806ddd2
Tomislav Radanović <tommy.radan@gmail.com>
2019-01-29 22:19:46 +0100

    Update licencing informaction and add default game modules

--- RenderingEngine/Renderables/Model.cpp
+++ RenderingEngine/Renderables/Model.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018 Tomislav Radanovic
+ * Copyright (c) 2015-2019 Tomislav Radanovic
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
